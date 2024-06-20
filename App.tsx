4758ac959c2e2a126f67ad70938eefb758009b04
Abdul-Rahman Ammourah <abdulrhmanammourah@gmail.com>
2024-06-20 18:11:28 +0300

    Some More Stuff

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { View, Text, Modal,StyleSheet } from "react-native";
 import Style from "./Style/Model";
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import Login from "./Pages/Signup";
+import Signup from "./Pages/Signup";
 import Home from "./Pages/Home";
 import { enableScreens } from 'react-native-screens';enableScreens();
 export default function App() {
@@ -17,6 +17,6 @@ export default function App() {
 
 
   return (
-    <Login></Login>
+    <Signup></Signup>
   );
 }
--- Pages/Signup.js
+++ Pages/Signup.js
@@ -1,29 +1,50 @@
 import React from 'react';
 import { View, Text, Image, TextInput, TouchableOpacity, StyleSheet, Modal } from 'react-native';
-import Title from '../Resources/TItleLogin.png';
+import Title from '../assets/WannaChatTitle.png';
 import C1Button from '../Functions/CustomButton1';
 export default function Login() {
     const [stats, setStats] = React.useState({
-        emailinput: '',
+        showUser: false,
         signupCont: false,
+        
+    });
+    const [user,setUser] = React.useState({
+        emailinput: '',
         username: '',
         age: '',
         password: '',
+        Id: 0
     });
 
-    const handleMailPress = () => {
+    const handleCreateNewAccountPress = () => {
         const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-        if (emailRegex.test(stats.emailinput)) {
+        if (emailRegex.test(user.emailinput)) {
             setStats({ ...stats, signupCont: true });
         } else {
             alert('Invalid email address');
         }
     };
+    const handleLoginPress = () => {
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (emailRegex.test(user.emailinput)) {
+            if (user.password.length >= 8) {
+                
+            }
+            else{
+                alert('Invalid email address or password ');
+            }
+        } else {
+            alert('Invalid email address or password ');
+        }
+
+    };
     const handleSignInPress = () => {
         const usernameRegex = /^[a-zA-Z0-9_]+$/;
         const ageRegex = /^[0-9]+$/;
-        if (usernameRegex.test(stats.username) && ageRegex.test(stats.age) && stats.password.length >= 8) {
+        if (usernameRegex.test(user.username) && ageRegex.test(user.age) && user.password.length >= 8) {
             alert('Valid username, age or password');
+            setStats({ ...stats, signupCont: false });
+
         } else {
             alert('Invalid username, age or password');
         }
@@ -33,25 +54,34 @@ export default function Login() {
         <View style={styles.container}>
             <Image source={Title} style={styles.imageCon} resizeMode="contain" />
 
-            <Text style={styles.createAccountText}>CREATE AN ACCOUNT</Text>
+            <Text style={styles.createAccountText}>Welcome to my Sweet little chatting App</Text>
 
-            <Text style={styles.instructionsText}>ENTER YOUR EMAIL TO SIGN UP FOR THIS APP</Text>
+            <Text style={styles.instructionsText}>Login or Create a new Account</Text>
 
             <TextInput
                 style={styles.input}
                 placeholder="example@domain.com"
                 placeholderTextColor="#fff"
-                onChangeText={(text) => setStats({ ...stats, emailinput: text })}
+                onChangeText={(text) => setUser({ ...user, emailinput: text })}
+            />
+            <TextInput
+                style={styles.input}
+                placeholder="Password"
+                secureTextEntry
+                placeholderTextColor="#fff"
+                onChangeText={(text) => setUser({ ...user, password: text })}
             />
 
-            <TouchableOpacity style={styles.button} onPress={handleMailPress}>
-                <Text style={styles.buttonText}>SIGN UP WITH EMAIL</Text>
+            <TouchableOpacity style={styles.button} onPress={handleLoginPress}>
+                <Text style={styles.buttonText}>Login</Text>
             </TouchableOpacity>
 
-            <Text style={styles.footerText}>
-                BY SIGNING UP, YOU AGREE TO OUR TERMS OF SERVICE AND PRIVACY POLICY
-            </Text>
+            <TouchableOpacity style={styles.button} onPress={handleCreateNewAccountPress}>
+                <Text style={styles.buttonText}>Create a New Account</Text>
+            </TouchableOpacity>
+
             <C1Button text='BackDoor' onPress={() => setStats({ ...stats, signupCont: true })}></C1Button>
+
             <Modal
                 visible={stats.signupCont}
                 animationType='slide'
@@ -64,34 +94,50 @@ export default function Login() {
                         <Text style={styles.modalText}>ENTER YOUR USER NAME</Text>
                         <TextInput
                             style={styles.input}
-                            placeholder="MAKE"
+                            placeholder="Mike"
                             placeholderTextColor="#fff"
-                            onChangeText={(text) => setStats({ ...stats, username: text })}
+                            onChangeText={(text) => setUser({ ...user, username: text })}
                         />
 
                         <Text style={styles.modalText}>ENTER YOUR AGE</Text>
                         <TextInput
                             style={styles.input}
-                            placeholder="AGE"
+                            placeholder="Age"
                             placeholderTextColor="#fff"
                             keyboardType='numeric'
-                            onChangeText={(text) => setStats({ ...stats, age: text })}
+                            onChangeText={(text) => setUser({ ...user, age: text })}
                             maxLength={3}
                         />
 
                         <Text style={styles.modalText}>ENTER YOUR PASSWORD</Text>
                         <TextInput
                             style={styles.input}
-                            placeholder="PASSWORD"
+                            placeholder="Password"
                             placeholderTextColor="#fff"
                             secureTextEntry
-                            onChangeText={(text) => setStats({ ...stats, password: text })}
+                            onChangeText={(text) => setUser({ ...user, password: text })}
                         />
 
                         <TouchableOpacity style={styles.button} onPress={handleSignInPress}>
                             <Text style={styles.buttonText}>CONTINUE</Text>
                         </TouchableOpacity>
+                        <Text style={styles.footerText}>
+                            BY SIGNING UP, YOU AGREE TO OUR TERMS OF SERVICE AND PRIVACY POLICY
+                        </Text>
                     </View>
+                    <C1Button text='BackDoor' onPress={() => setStats({ ...stats, showUser: true })}></C1Button>
+                </View>
+            </Modal>
+            <Modal
+                visible={stats.showUser}
+                animationType='slide'
+                transparent={false}
+            >
+                <View style={styles.modalOuter}>
+                    <Text style={styles.createAccountText}>your user name is: {user.username}</Text>
+                    <Text style={styles.createAccountText}>your age is:{user.age}</Text>
+                    <Text style={styles.createAccountText}>your password is: {user.password}</Text>
+
                 </View>
             </Modal>
         </View>
@@ -116,16 +162,18 @@ const styles = StyleSheet.create({
         fontWeight: 'bold',
         textAlign: 'center',
         marginBottom: 10,
+        color: '#000',
     },
     instructionsText: {
         fontSize: 14,
         textAlign: 'center',
         marginBottom: 20,
+        color: '#000',
     },
     input: {
         width: '100%',
         height: 50,
-        backgroundColor: '#0052cc',
+        backgroundColor: '#000',
         borderRadius: 5,
         paddingHorizontal: 10,
         color: '#fff',
@@ -135,7 +183,7 @@ const styles = StyleSheet.create({
     button: {
         width: '100%',
         height: 50,
-        backgroundColor: '#0052cc',
+        backgroundColor: 'linear-gradient(0deg, rgba(0,0,0,1) 0%, rgba(255,255,255,1) 100%)',
         borderRadius: 5,
         justifyContent: 'center',
         alignItems: 'center',
@@ -145,6 +193,7 @@ const styles = StyleSheet.create({
         color: '#fff',
         fontSize: 16,
         fontWeight: 'bold',
+
     },
     footerText: {
         fontSize: 12,
@@ -167,6 +216,6 @@ const styles = StyleSheet.create({
     modalText: {
         fontSize: 14,
         marginBottom: 10,
+        color: '#000',
     },
 });
-
